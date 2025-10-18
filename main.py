19ba6172fa91aeddeb9f09c936ee140a77416e0b
Vadimchik <tumanov.vr@gmail.com>
2025-10-18 22:10:00 +0300

    Development: initial screen

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-import tkinter
+import tkinter as tk
+
 
 class Rusish:
-    def __init__(self,root):
+    def __init__(self, root:tk.Tk):
         self.root = root
-    def osnova(self):
-        pass
+
+        self.seting()
+        self.spce()
+        
+    def seting(self):
+        self.root.title("Помошник для ЕГЭ по Русскому")
+        
+        widh = 1000
+        heig = 600
+
+        screen_widh = root.winfo_screenwidth()
+        screen_heig = root.winfo_screenheight()
+
+        x = (screen_widh - widh) // 2
+        y = (screen_heig - heig) // 2
+        self.root.geometry(f"{widh}x{heig}+{x}+{y}")
+        
+    def space(self):
+        self.up_main_screen = tk.Frame(self.root, )
+
     
 if __name__ == "__main__":
-    root = tkinter.Tk()
+    root = tk.Tk()
     rusish = Rusish(root)
     root.mainloop()
