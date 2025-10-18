8e2aefe79bd3725d0a66b80accca4408d83a4caf
Vadimchik <tumanov.vr@gmail.com>
2025-10-18 20:45:20 +0300

    Assistant for the ЕГЭ in Russian

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter
+
+class Rusish:
+    def __init__(self,root):
+        self.root = root
+    def osnova(self):
+        pass
+    
+if __name__ == "__main__":
+    root = tkinter.Tk()
+    rusish = Rusish(root)
+    root.mainloop()
